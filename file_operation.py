1ce65cb6bf8b4dba7433a588b0acbef566a3532e
Lovingmage <skiphoopwow@gmail.com>
2015-03-03 20:22:05 +0800

    rewrite
     rewrite

--- file_operation.py
+++ file_operation.py
@@ -1,7 +1,15 @@
 #This is the test case operation script
+# Split test cases by ' and then save each splited test cases into new list
 
-test_case_obj = open('oracle.fuzz.txt')
-test_case_temp = test_case_obj.readline()
-test_case_temp = test_case_temp[:-1]  #remove the last char, which is '\n'
-test_case_split = test_case_temp.split(' ')
-print test_case_split
+def fileopt(filename):
+    list = []
+    test_case_obj = open(filename)
+    test_case_temp = test_case_obj.readline()
+    while test_case_temp :
+        test_case_temp = test_case_temp[:-1]  #remove the last char, which is '\n'
+        test_case_split = test_case_temp.split(' ')
+        #   print test_case_split
+        list.append(test_case_split)
+        test_case_temp = test_case_obj.readline()
+        return list
+
