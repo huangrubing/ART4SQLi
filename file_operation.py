c3a55042f15d8fa9071fd2e4b1893a066182dd91
Lovingmage <skiphoopwow@gmail.com>
2015-03-05 17:28:37 +0800

    Add modified file by all template
    Add modified file by all template

--- file_operation.py
+++ file_operation.py
@@ -1,7 +1,10 @@
-#This is the test case operation script
-# Split test cases by ' and then save each splited test cases into new list
+#   File Operation Package
+#   Author : C.H. Wang
+#   Department of Computer Science at Harbin Engineering University
+#   This is the test case operation script
+#   Split test cases by ' and then save each splited test cases into new list
 
-def fileopt(filename):
+def fileopt_line(filename):
     list = []
     test_case_obj = open(filename)
     test_case_temp = test_case_obj.readline()
@@ -10,6 +13,18 @@ def fileopt(filename):
         test_case_split = test_case_temp.split(' ')
         #   print test_case_split
         list.append(test_case_split)
+        test_case_temp = test_case_obj.readline()
+        return list
+
+def fileopt_all(filename):
+    list = []
+    test_case_obj = open(filename)
+    test_case_temp = test_case_obj.readline()
+    while test_case_temp :
+        test_case_temp = test_case_temp[:-1]  #remove the last char, which is '\n'
+        test_case_split = test_case_temp.split(' ')
+        #   print test_case_split
+        list = list + test_case_split
         test_case_temp = test_case_obj.readline()
         return list
 
