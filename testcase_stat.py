46689ea70507a2230d660b467fe055856cbb1f60
Lovingmage <skiphoopwow@gmail.com>
2015-03-05 21:01:47 +0800

    Done of Test Case Stat
    Done of Test Case Stat

--- testcase_stat.py
+++ testcase_stat.py
@@ -4,14 +4,20 @@
 #   This is the test case statistic script
 #   Split test cases by ' and then save each splited test cases into new list
 
-list = ['alice', 'dod', 'alice', 'bob', 'alice', 'tom']
-testcase_stat = {list[0]:1}
-for count i in range(1, len(list), 1):
-    print testcase_stat
-    del testcase_stat[list[0]]
-        #for count in (1, len(list), 1):
-        #print count
-            #if list[count] == list[0]:
-            #print list[count]
-            #testcase_stat[list[count]] = testcase_stat[list[count]] + 1
-            #del list[count]
+#list = ['alice', 'dod', 'alice', 'bob', 'alice', 'tom']
+
+def stat(list):
+    testcase_stat = {list[0]:0}
+    list2 = []
+    for count_i in range(0, len(list), 1):
+        if list[count_i] not in testcase_stat.keys():
+            testcase_stat[list[count_i]] = 1
+        else:
+            testcase_stat[list[count_i]] += 1
+    return  testcase_stat
+#        for count in (1, len(list), 1):
+#        print count
+#            if list[count] == list[0]:
+#            print list[count]
+#            testcase_stat[list[count]] = testcase_stat[list[count]] + 1
+#            del list[count]
