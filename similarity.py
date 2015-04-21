7636119a5b23c34a2acf691ab14b8a19795ecd80
Lovingmage <skiphoopwow@gmail.com>
2015-04-21 22:15:52 +0800

    0
    0

--- similarity.py
+++ similarity.py
@@ -39,31 +39,64 @@ def sim2(vector1, vector2):
 		inner_product += vector1[index] * vector2[index]
 	return inner_product / (math.sqrt(norm1) * math.sqrt(norm2))
 
-def 
+def search_candidate(candidate_set, selected_set, test_case_feature_list):
+	sim_inner = 1
+	sim_outer = 0
+	min_dis = 0
+	candidate = -1
+	for index_c in candidate_set :
+		for index_s in selected_set:
+			temp = sim2(test_case_feature_list[index_c], test_case_feature_list[index_s])
+			#print temp
+			if  temp <= sim_inner:
+				min_dis = temp
+				#print min_dis
+		#print min_dis
+		if min_dis >= sim_outer:
+			sim_outer = min_dis
+			candidate = index_c 
+			#print candidate
+	return candidate
+
+
 
 
 def fscs(test_case_feature_list):
-	selected_set = []
-	candidate_set = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
-	selection_seed = -1
-	candidate = 0
-	while len(selected_set) < len(test_case_feature_list)-11:
+	selected_set = [5]
+	candidate_set = []
+	for i in range(0,10,1):
+		temp = random.randint(0,len(test_case_feature_list)-1)
+		while  temp in candidate_set:
+			temp = random.randint(0,len(test_case_feature_list)-1)
+		candidate_set.append(temp)
+	print candidate_set
+	candidate = search_candidate(candidate_set, selected_set, test_case_feature_list)
+	selected_set.append(candidate)
+	print selected_set
 
-		for i in range(0,10,1):
-			while selection_seed in selected_set or selection_seed in candidate_set:
-				selection_seed = random.randint(0,len(test_case_feature_list))
-				candidate_set[i] = selection_seed
-		candidate = search_candidate(candidate_set, selected_set)
-		selected_set.append(candidate)
-
-	return selected_set
 
 
 		
-"""
+
 if __name__=="__main__":
+
 	v1 = {'alice':10, 'bob':3.272, 'tom':1.149}
 	v2 = {'alice':1.17, 'bob':2.272, 'tom':3.149}
-	dis = sim2(v1,v2)
-	print dis
-"""
+	v3 = {'alice':12, 'bob':2.272, 'tom':0.149}
+	v4 = {'alice':13.17, 'bob':3.272, 'tom':1.149}
+	#candidate_set_test = [0,1,2,3]
+	v5 = {'alice':9, 'bob':6.272, 'tom':1.149}
+	v6 = {'alice':1.17, 'bob':13.272, 'tom':11.149}
+	v7 = {'alice':10, 'bob':3.272, 'tom':1.149}
+	v8 = {'alice':1.17, 'bob':2.272, 'tom':3.149}
+	v9 = {'alice':12, 'bob':2.272, 'tom':0.149}
+	v10 = {'alice':13.17, 'bob':3.272, 'tom':1.149}
+	v11 = {'alice':10, 'bob':3.272, 'tom':1.149}
+	v12 = {'alice':1.17, 'bob':2.272, 'tom':3.149}
+	v13 = {'alice':12, 'bob':2.272, 'tom':0.149}
+	v14 = {'alice':13.17, 'bob':3.272, 'tom':1.149}
+	#selected_set_test = [4,5]
+	test_case_feature_list = [v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14]
+	fscs(test_case_feature_list)
+	#candidate = search_candidate(candidate_set_test, selected_set_test, test_case_feature_list)
+	#print candidate
